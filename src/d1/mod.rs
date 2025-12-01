c10e74694f28a51daa82506917d5b249a7db055e
Lars Schumann <LarsSchumann@mail.de>
2025-12-01 16:23:39 +0100

    whoops

---
+++ src/d1/mod.rs
@@ -0,0 +1,20 @@
+pub fn p1() {
+    let input = include_str!("./p1.txt");
+    let mut dial_location = 50;
+    let mut zero_count = 0;
+
+    for rotation in input.lines() {
+        let mut rotation = rotation.chars();
+        let sign = match rotation.next().unwrap() {
+            'R' => 1,
+            'L' => -1,
+            _ => panic!(),
+        };
+        let amount = rotation.as_str().parse::<i32>().unwrap();
+        dial_location = (dial_location + sign * amount) % 100;
+        if dial_location == 0 {
+            zero_count += 1;
+        }
+    }
+    println!("{zero_count}");
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#![deny(clippy::pedantic, clippy::nursery)]
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::missing_panics_doc)]
+
+#[allow(dead_code)]
+mod d1;
+
+fn main() {
+    d1::p1();
+}
