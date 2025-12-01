5d65373633cece06d8c4cb19b7adea78f8365eda
Lars Schumann <LarsSchumann@mail.de>
2025-12-01 16:27:31 +0100

    d1p2

--- src/d1/mod.rs
+++ src/d1/mod.rs
@@ -18,3 +18,27 @@ pub fn p1() {
     }
     println!("{zero_count}");
 }
+
+pub fn p2() {
+    let input = include_str!("./p2.txt");
+    let mut dial_location = 50;
+    let mut zero_count = 0;
+
+    for rotation in input.lines() {
+        let mut rotation = rotation.chars();
+        let sign = match rotation.next().unwrap() {
+            'R' => 1,
+            'L' => -1,
+            _ => panic!(),
+        };
+        let amount = rotation.as_str().parse::<i32>().unwrap();
+
+        for _ in 0..amount {
+            dial_location = (dial_location + sign) % 100;
+            if dial_location == 0 {
+                zero_count += 1;
+            }
+        }
+    }
+    println!("{zero_count}");
+}
