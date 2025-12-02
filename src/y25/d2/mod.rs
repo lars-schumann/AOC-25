f11c3d953fc7ab7ea9f030c8b2d5aef039b3f5bd
Lars Schumann <LarsSchumann@mail.de>
2025-12-02 14:07:39 +0100

    d2p1 iterator solution

--- src/y25/d2/mod.rs
+++ src/y25/d2/mod.rs
@@ -8,7 +8,7 @@ fn parse_input(input: &str) -> impl Iterator<Item = RangeInclusive<u128>> {
         .map(|r| r.0.parse::<u128>().unwrap()..=r.1.parse().unwrap())
 }
 
-pub fn p1() {
+pub fn p1() -> u128 {
     let ranges = parse_input(include_str!("./p1.txt"));
     let mut invalid_id_sum = 0;
 
@@ -24,10 +24,29 @@ pub fn p1() {
             }
         }
     }
-    println!("{invalid_id_sum}");
+    invalid_id_sum
 }
 
-pub fn p2() {
+pub fn p1_iterator() -> u128 {
+    let ranges = parse_input(include_str!("./p1.txt"));
+
+    ranges
+        .flat_map(|range| {
+            range.filter(|num| {
+                let num_str = num.to_string();
+                let num_str_len = num_str.len();
+                if num_str_len.is_multiple_of(2) {
+                    let (a, b) = num_str.split_at(num_str_len / 2);
+                    a == b
+                } else {
+                    false
+                }
+            })
+        })
+        .sum()
+}
+
+pub fn p2() -> u128 {
     let ranges = parse_input(include_str!("./p2.txt"));
     let mut invalid_id_sum: u128 = 0;
 
@@ -51,9 +70,25 @@ pub fn p2() {
             invalid_id_sum += invalid_ids.iter().sum::<u128>();
         }
     }
-    println!("{invalid_id_sum}");
+    invalid_id_sum
 }
 
 fn divisors(num: usize) -> impl Iterator<Item = usize> {
     (1..=num).filter(move |div| num.is_multiple_of(*div))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_p1() {
+        assert_eq!(p1(), 23_560_874_270);
+        assert_eq!(p1_iterator(), 23_560_874_270);
+    }
+
+    #[test]
+    fn test_p2() {
+        assert_eq!(p2(), 44_143_124_633);
+    }
 }
