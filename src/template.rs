0a299e5eee79f70eb36faf06bbe6f904e895905a
Lars Schumann <LarsSchumann@mail.de>
2025-12-06 16:30:07 +0100

    add template

---
+++ src/template.rs
@@ -0,0 +1,28 @@
+fn parse_input(input: &str) -> () {
+    todo!()
+}
+
+fn p1(input: &str) -> () {
+    let input = parse_input(input);
+    todo!()
+}
+
+fn p2(input: &str) -> () {
+    let input = parse_input(input);
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_p1() {
+        assert_eq!(p1(include_str!("./input.txt")), ());
+    }
+
+    #[test]
+    fn test_p2() {
+        assert_eq!(p2(include_str!("./input.txt")), ());
+    }
+}
