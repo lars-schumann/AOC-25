3b21a31c715e73fe7dac87515afc8422fb053ece
Lars Schumann <LarsSchumann@mail.de>
2025-12-12 19:51:44 +0100

    day 12 start trying to git gud

--- src/y25/d12/mod.rs
+++ src/y25/d12/mod.rs
@@ -1,5 +1,4 @@
-#![feature(iter_advance_by)]
-fn p1(input: &str) -> i64 {
+fn p1(input: &[u8]) -> i64 {
     fn num_from_bytes(bytes: &[u8]) -> i64 {
         let mut num: i64 = 0;
         let mut multiplier = 1;
@@ -10,21 +9,23 @@ fn p1(input: &str) -> i64 {
         num
     }
 
-    let mut lines = input.lines();
-    lines.advance_by(30).unwrap();
+    const LINE_WIDTH: usize = 25;
+
+    let relevant = &input[(16 * 6)..];
 
     let mut sum = 0;
 
-    for line in lines {
-        let w = num_from_bytes(&line.as_bytes()[0..2]);
-        let h = num_from_bytes(&line.as_bytes()[3..5]);
+    for i in 0..1000 {
+        let s = i * LINE_WIDTH; //current line starting index
+        let w = num_from_bytes(&relevant[(s + 0)..(s + 2)]);
+        let h = num_from_bytes(&relevant[(s + 3)..(s + 5)]);
 
-        let n1 = num_from_bytes(&line.as_bytes()[7..9]);
-        let n2 = num_from_bytes(&line.as_bytes()[10..12]);
-        let n3 = num_from_bytes(&line.as_bytes()[13..15]);
-        let n4 = num_from_bytes(&line.as_bytes()[16..18]);
-        let n5 = num_from_bytes(&line.as_bytes()[19..21]);
-        let n6 = num_from_bytes(&line.as_bytes()[22..24]);
+        let n1 = num_from_bytes(&relevant[(s + 7)..(s + 9)]);
+        let n2 = num_from_bytes(&relevant[(s + 10)..(s + 12)]);
+        let n3 = num_from_bytes(&relevant[(s + 13)..(s + 15)]);
+        let n4 = num_from_bytes(&relevant[(s + 16)..(s + 18)]);
+        let n5 = num_from_bytes(&relevant[(s + 19)..(s + 21)]);
+        let n6 = num_from_bytes(&relevant[(s + 22)..(s + 24)]);
 
         let block_count: i64 = [n1, n2, n3, n4, n5, n6].iter().sum();
 
@@ -42,6 +43,6 @@ mod tests {
 
     #[test]
     fn test_p1() {
-        assert_eq!(p1(include_str!("./input.txt")), 0);
+        assert_eq!(p1(include_bytes!("./input.txt")), 0);
     }
 }
