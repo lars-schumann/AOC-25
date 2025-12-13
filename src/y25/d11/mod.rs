7211e059a1a411f17c1c1741ea94b799e2cae7cf
Lars Schumann <LarsSchumann@mail.de>
2025-12-13 21:04:14 +0100

    day 11 part 1

--- src/y25/d11/mod.rs
+++ src/y25/d11/mod.rs
@@ -1,4 +1,6 @@
-fn p1(input: &str) -> () {
+fn p1(input: &str) -> u64 {
+    const YOU: [u8; 3] = [b'y', b'o', b'u'];
+    const OUT: [u8; 3] = [b'o', b'u', b't'];
     let input = &input[..input.len() - 1];
 
     let adj: Vec<([u8; 3], Vec<[u8; 3]>)> = input
@@ -22,7 +24,22 @@ fn p1(input: &str) -> () {
         })
         .collect();
 
-    todo!()
+    #[allow(clippy::items_after_statements)]
+    fn find(starting: [u8; 3], adj: &[([u8; 3], Vec<[u8; 3]>)]) -> u64 {
+        if starting == OUT {
+            return 1;
+        }
+
+        adj.iter()
+            .find(|elem| elem.0 == starting)
+            .map(|elem| elem.1.clone())
+            .unwrap_or_default()
+            .iter()
+            .map(|kid| find(*kid, adj))
+            .sum()
+    }
+
+    find(YOU, &adj)
 }
 
 fn p2(input: &str) -> () {
@@ -36,7 +53,7 @@ mod tests {
 
     #[test]
     fn test_p1() {
-        assert_eq!(p1(include_str!("./input.txt")), ());
+        assert_eq!(p1(include_str!("./input.txt")), 0);
     }
 
     #[test]
