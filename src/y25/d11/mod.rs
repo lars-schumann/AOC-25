ceea77a88f4df3d46b2cbe040f0119960b1dd7b5
Lars Schumann <LarsSchumann@mail.de>
2025-12-14 19:05:57 +0100

    day 11 not much progress

--- src/y25/d11/mod.rs
+++ src/y25/d11/mod.rs
@@ -1,66 +1,85 @@
-use std::{collections::HashMap, hash::Hash};
+#![allow(unsafe_op_in_unsafe_fn)]
+#![allow(static_mut_refs)]
 
-const YOU: &[u8; 3] = b"you";
-const OUT: &[u8; 3] = b"out";
-const SVR: &[u8; 3] = b"svr";
-const DAC: &[u8; 3] = b"dac";
-const FFT: &[u8; 3] = b"fft";
+const YOU: u16 = node_name_to_u16(*b"you");
+const OUT: u16 = node_name_to_u16(*b"out");
+const SVR: u16 = node_name_to_u16(*b"svr");
+const DAC: u16 = node_name_to_u16(*b"dac");
+const FFT: u16 = node_name_to_u16(*b"fft");
 
-fn parse_input(input: &str) -> Vec<([u8; 3], Vec<[u8; 3]>)> {
-    input
-        .lines()
+unsafe fn parse_input(input: &[u8]) -> Vec<(u16, Vec<u16>)> {
+    input[..input.len() - 1]
+        .split(|&b| b == b'\n')
         .map(|line| {
-            let mut chunks = line.split_ascii_whitespace();
-            let first: [u8; 3] = chunks
-                .next()
-                .unwrap()
-                .trim_suffix(':')
-                .bytes()
-                .collect::<Vec<u8>>()
-                .try_into()
-                .unwrap();
-
-            let tail: Vec<[u8; 3]> = chunks
-                .map(|elem| elem.bytes().collect::<Vec<u8>>().try_into().unwrap())
+            let first = node_name_to_u16([
+                *line.get_unchecked(0),
+                *line.get_unchecked(1),
+                *line.get_unchecked(2),
+            ]);
+            let tail = line[5..]
+                .split(|&b| b == b' ')
+                .map(|name| {
+                    node_name_to_u16([
+                        *name.get_unchecked(0),
+                        *name.get_unchecked(1),
+                        *name.get_unchecked(2),
+                    ])
+                })
                 .collect();
-
             (first, tail)
         })
         .collect()
 }
 
-fn p1(input: &str) -> u64 {
+const fn node_name_to_u16(value: [u8; 3]) -> u16 {
+    let a = (value[0] - b'a') as u16;
+    let b = (value[1] - b'a') as u16;
+    let c = (value[2] - b'a') as u16;
+
+    (a << 10) | (b << 5) | c
+}
+
+unsafe fn p1(input: &[u8]) -> u16 {
+    static mut MEMO: [u16; (u16::MAX / 2) as usize] = [u16::MAX; (u16::MAX / 2) as usize];
     let adj = parse_input(input);
 
     #[allow(clippy::items_after_statements)]
-    fn find(starting: [u8; 3], adj: &[([u8; 3], Vec<[u8; 3]>)]) -> u64 {
-        if starting == *OUT {
+    unsafe fn find(starting: u16, adj: &[(u16, Vec<u16>)]) -> u16 {
+        if starting == OUT {
             return 1;
         }
 
         adj.iter()
             .find(|elem| elem.0 == starting)
             .map(|elem| elem.1.clone())
-            .unwrap_or_default()
+            .unwrap_unchecked()
             .iter()
-            .map(|kid| find(*kid, adj))
+            .map(|kid| {
+                let x = *MEMO.get_unchecked(*kid as usize);
+                if x != u16::MAX {
+                    return x;
+                }
+                let y = find(*kid, adj);
+                *MEMO.get_unchecked_mut(*kid as usize) = y;
+                y
+            })
             .sum()
     }
 
-    find(*YOU, &adj)
+    find(YOU, &adj)
 }
 
-fn p2(input: &str) -> u64 {
+unsafe fn p2(input: &[u8]) -> u64 {
     let adj = parse_input(input);
 
-    let mut the_holy_counter = HashMap::<[u8; 3], u64>::new();
+    let mut the_holy_counter = std::collections::HashMap::<u16, u64>::new();
 
     #[allow(clippy::items_after_statements)]
     fn find(
-        start: [u8; 3],
-        goal: [u8; 3],
-        holy_counter: &mut HashMap<[u8; 3], u64>,
-        adj: &[([u8; 3], Vec<[u8; 3]>)],
+        start: u16,
+        goal: u16,
+        holy_counter: &mut std::collections::HashMap<u16, u64>,
+        adj: &[(u16, Vec<u16>)],
     ) -> u64 {
         if start == goal {
             return 1;
@@ -83,11 +102,11 @@ fn p2(input: &str) -> u64 {
             .sum()
     }
 
-    let a = find(*SVR, *FFT, &mut the_holy_counter, &adj);
+    let a = find(SVR, FFT, &mut the_holy_counter, &adj);
     the_holy_counter.clear();
-    let b = find(*FFT, *DAC, &mut the_holy_counter, &adj);
+    let b = find(FFT, DAC, &mut the_holy_counter, &adj);
     the_holy_counter.clear();
-    let c = find(*DAC, *OUT, &mut the_holy_counter, &adj);
+    let c = find(DAC, OUT, &mut the_holy_counter, &adj);
     a * b * c
 }
 
@@ -97,11 +116,14 @@ mod tests {
 
     #[test]
     fn test_p1() {
-        assert_eq!(p1(include_str!("./input.txt")), 772);
+        assert_eq!(unsafe { p1(include_bytes!("./input.txt")) }, 772);
     }
 
     #[test]
     fn test_p2() {
-        assert_eq!(p2(include_str!("./input.txt")), 423_227_545_768_872);
+        assert_eq!(
+            unsafe { p2(include_bytes!("./input.txt")) },
+            423_227_545_768_872
+        );
     }
 }
